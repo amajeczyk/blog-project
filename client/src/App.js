09514bc02e0421134d9dffd5e69e64e7012731e5
amajeczyk <amajerczyk414@gmail.com>
2022-02-03 20:45:05 +0100

    modified App.js
    Added methods to call server for autehntication

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,49 @@
-import React from 'react';
+import React  from 'react';
 import './App.css';
-import Footer from './components/Footer';
-import MainContent from './components/MainContent';
-import NavBar from './components/NavBar';
 
-function App() {
-  return (
-  <div className='main-wrapper'>
-    <NavBar/>
-    <MainContent/>
-    <Footer />
-  </div>)
+import {
+  Routes,
+  Route,
+} from "react-router-dom";
+import  Login from './components/login/Login';
+import LandingPage from './LandingPage';
+import Registration from './components/login/Registration';
+import { authenticationService } from './_services/authentication.service';
+
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      user : null,
+      userIsFetched : false
+    }
+  }
+
+  setCurrenUser = () => {
+    console.log('setCurrenUser')
+    authenticationService.authenticateRequest().then(result => {
+      this.setState( {user : result, userIsFetched : true} );
+    });
+  }
+
+  componentDidMount() {
+    this.setCurrenUser();
+  }
+
+
+  render()
+  {
+    return (
+      <div className='main-wrapper'>      
+        <Routes>
+          <Route path='/register' element={<Registration/>}/>
+          <Route path='/login'  element={<Login setCurrenUser={this.setCurrenUser}/>}/>
+          {this.state.userIsFetched  && <Route path='/' element={<LandingPage user={this.state.user} setCurrenUser={this.setCurrenUser}/>}/> } 
+        </Routes>
+      </div>)
+  }
+ 
 }
 
 export default App;
